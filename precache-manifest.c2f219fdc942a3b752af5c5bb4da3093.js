0b4b438118831f1ac2ef86e2dbc9c4ac2778ce63
Helen Sabrina Braz <helsabrinacarvalho@hotmail.com>
2020-06-22 19:07:24 -0300

    Updates

---
+++ precache-manifest.c2f219fdc942a3b752af5c5bb4da3093.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a5464839ccede23c4540d8a27ddf801",
+    "url": "/discord-ui-clone/index.html"
+  },
+  {
+    "revision": "6041b9ec690cf69561bf",
+    "url": "/discord-ui-clone/static/js/2.a31c7468.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/discord-ui-clone/static/js/2.a31c7468.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d9683397a66d97c9cd0d",
+    "url": "/discord-ui-clone/static/js/main.3f1e254d.chunk.js"
+  },
+  {
+    "revision": "c595eb5491dee2ee89ee",
+    "url": "/discord-ui-clone/static/js/runtime-main.dbdfda64.js"
+  },
+  {
+    "revision": "7cc4a22904037b240932687125e01a97",
+    "url": "/discord-ui-clone/static/media/Logo.7cc4a229.svg"
+  },
+  {
+    "revision": "5c35f178d6d4f8fbeea771dd6e78af66",
+    "url": "/discord-ui-clone/static/media/me.5c35f178.svg"
+  }
+]);
